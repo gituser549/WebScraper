ad3b3804a82e4f77d9ccc06d68f0918db6dec5c6
Yaroslav Parfenov <mailpost336@gmail.com>
2026-02-24 16:17:17 +0300

    Unnecessary comments removed from Main.java

--- src/main/java/com/parfyonoff/webscraper/Main.java
+++ src/main/java/com/parfyonoff/webscraper/Main.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
         if (args.length >= 4) {
             String fileName;
             List<String> apiNames = new ArrayList<>();
@@ -33,29 +31,5 @@ public class Main {
             CliRunner cliRunner = new CliRunner();
             cliRunner.start();
         }
-        //CliRunner cliRunner = new CliRunner();
-        //cliRunner.run(List.of("hn", "ex", "hh"), "/Users/mac/OOP_Labs/WebScraper/src/test.json", true, "all");
-
-        /*
-        Fetcher fetcher = new Fetcher(new ObjectMapper(), HttpClient.newHttpClient());
-        ExchangeScraper exchangeScraper = new ExchangeScraper(fetcher);
-
-
-        ExchangeResponseDto exchangeResponseDto = exchangeScraper.fetchToDTO();
-
-        List<APIClient<?>> apiClientsList = List.of(new HackerNewsScraper(fetcher), new ExchangeScraper(fetcher), new HeadHunterScraper(fetcher));
-        Service service = new Service(List.of("HackerNews", "Exchange", "HeadHunter"), apiClientsList);
-
-        JsonWriter.append(new File("/Users/mac/OOP_Labs/WebScraper/src/test.json"), service.fetchAsAggregatedType("HeadHunter"));
-
-        HeadHunterScraper headHunterScraper = new HeadHunterScraper(fetcher);
-
-        HeadHunterResponseDto headHunterResponseDto = headHunterScraper.fetchToDTO();
-        AggregatedData aggregatedData = new AggregatedData((int) (Math.random() * 10), "HeadHunter",  Instant.now().toString(), headHunterResponseDto);
-
-
-        JsonWriter.append(new File("/Users/mac/OOP_Labs/WebScraper/src/test.json"), aggregatedData);
-        CsvWriter.append(new File("/Users/mac/OOP_Labs/WebScraper/src/test.csv"), headHunterScraper.fetchToMap());
-    */
     }
 }
